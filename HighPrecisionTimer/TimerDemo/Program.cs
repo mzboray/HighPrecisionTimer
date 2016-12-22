047786ec5af7c89c4af21b0bfcd3452a6d7b68bf
Mike Zboray <mzboray@users.noreply.github.com>
2016-12-21 21:32:45 -0800

    add timing summary to demo

--- HighPrecisionTimer/TimerDemo/Program.cs
+++ HighPrecisionTimer/TimerDemo/Program.cs
@@ -13,65 +13,130 @@ namespace TimerDemo
     {
         static void Main(string[] args)
         {
-            TestThreadingTimer();
-            TestMultimediaTimer();
-            TestTask();
+            var threadingResult = TestThreadingTimer();
+            var timerResult = TestMultimediaTimer();
+            var taskResult = TestTask();
+
+            Console.WriteLine($"Threading timer: {threadingResult.Summary()}");
+            Console.WriteLine($"Multimedia timer: {timerResult.Summary()}");
+            Console.WriteLine($"MultimediaTimer.Delay: {taskResult.Summary()}");
         }
 
-        private static void TestMultimediaTimer()
+        private static TimingResult TestMultimediaTimer()
         {
+            TimeSpan total = TimeSpan.Zero;
+            int iterations = 0;
             Stopwatch s = new Stopwatch();
             using (var timer = new MultimediaTimer() { Interval = 1 })
             {
-                timer.Elapsed += (o, e) => Console.WriteLine(s.ElapsedMilliseconds);
+                timer.Elapsed += (o, e) =>
+                {
+                    var ts = s.Elapsed;
+                    lock (s)
+                    {
+                        total += ts;
+                        iterations++;
+                    }
+                    Console.WriteLine(ts.TotalMilliseconds);
+                };
                 s.Start();
                 timer.Start();
                 Console.ReadKey(true);
                 timer.Stop();
             }
+
+            lock (s)
+            {
+                return new TimingResult()
+                {
+                    Elapsed = s.Elapsed,
+                    Iterations = iterations
+                };
+            }
         }
 
-        private static void TestThreadingTimer()
+        private static TimingResult TestThreadingTimer()
         {
+            TimeSpan total = TimeSpan.Zero;
+            int iterations = 0;
             Stopwatch s = new Stopwatch();
-            using (var timer = new Timer(o => Console.WriteLine(s.ElapsedMilliseconds), null, 0, 1))
+            using (var timer = new Timer(o =>
+            {
+                var ts = s.Elapsed;
+                lock (s)
+                {
+                    total += ts;
+                    iterations++;
+                }
+                Console.WriteLine(ts.TotalMilliseconds);
+
+            }, null, 0, 1))
             {
                 s.Start();
                 Console.ReadKey(true);
             }
+
+            lock (s)
+            {
+                return new TimingResult()
+                {
+                    Elapsed = s.Elapsed,
+                    Iterations = iterations
+                };
+            }
         }
 
-        private static void TestTask()
+        private static TimingResult TestTask()
         {
             try
             {
                 using (var cancelled = new CancellationTokenSource())
                 {
                     Console.CancelKeyPress += (o, e) => { cancelled.Cancel(); };
-                    TestTaskInner(cancelled.Token).GetAwaiter().GetResult();
+                    var result = TestTaskInner(cancelled.Token).GetAwaiter().GetResult();
+                    return result;
                 }
             }
             catch (OperationCanceledException)
             {
+                return default(TimingResult);
             }
         }
 
-        private static async Task TestTaskInner(CancellationToken token)
+        private static async Task<TimingResult> TestTaskInner(CancellationToken token)
         {
             Stopwatch s = Stopwatch.StartNew();
             TimeSpan prevValue = TimeSpan.Zero;
             int i = 0;
             while (true)
             {
-                Console.WriteLine(s.ElapsedMilliseconds);
+                Console.WriteLine(s.Elapsed.TotalMilliseconds);
                 await MultimediaTimer.Delay(1, token);
                 if (Console.KeyAvailable)
                 {
-                    return;
+                    break;
                 }
 
                 i++;
             }
+
+            return new TimingResult()
+            {
+                Elapsed = s.Elapsed,
+                Iterations = i
+            };
+        }
+    }
+
+    struct TimingResult
+    {
+        public TimeSpan Elapsed;
+
+        public int Iterations;
+
+        public string Summary()
+        {
+            return $"{Elapsed.TotalMilliseconds / Iterations} ms / iteration";
         }
     }
 }
